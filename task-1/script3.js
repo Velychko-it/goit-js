6f2c90646d27aa444e99d14020b005fd307f6f6d
Velychko-it <velihkovs1995@ukr.net>
2020-03-03 16:46:53 +0200

    init

--- task-1/script3.js
+++ task-1/script3.js
@@ -7,11 +7,12 @@ let message;
 const password = prompt("Please enter password:");
 
 if (password === null) {
+  console.log('work')
   message = "canceled by user";
 } else if (password === ADMIN_PASSWORD) {
   message = "welcome";
 } else {
   message = "wrong password ";
-}
+};
 
 alert(message);
---
+++ task-1/script5.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let price;
+let country;
+let message;
+
+country = prompt("Укажите страну доставки");
+country = country.toUpperCase();
+
+switch (country) {
+  case "CHINA":
+    price = 100;
+    break;
+
+  case "CHILI":
+    price = 250;
+    break;
+
+  case "AUSTRALIA":
+    price = 170;
+    break;
+
+  case "INDIA":
+    price = 80;
+    break;
+
+  case "JAMAIKA":
+    price = 120;
+    break;
+
+    default:
+        alert('доставка сюда не осуществляеться ');
+}
+
+alert(`доствка в  ${country} будет стоить ${price} кредитов `);
