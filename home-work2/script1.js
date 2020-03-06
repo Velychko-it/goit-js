11897b29ba76c4db7e0cde4549406c64b70b2ab8
Velychko-it <velihkovs1995@ukr.net>
2020-03-06 17:22:35 +0200

    init

--- home-work2/script1.js
+++ home-work2/script1.js
@@ -1,20 +1,10 @@
 const logItems = function(array) {
-  // const words =array.split(' ')
-  // console.log(array);
-
   for (let i = 0; i < array.length; i += 1) {
-    // console.log(i+1);
-    // console.log(array[i])
-    console.log(`${i + 1} - ${array[i]}...`);
+    console.log(`${i + 1} - ${array[i]}`);
+
   }
 };
-const logptems = function(numbers) {
-  for (let i = 0; i < numbers.length; i++) {
-    console.log(`${i + 1} - ${numbers[i]}`);
-  }
-};
+
 
 logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
-logptems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
-
-
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
